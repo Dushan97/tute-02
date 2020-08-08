77ebb03f9be1d52637866cc4dbead498c358d209
it19029900 <it19029900@my.sliit.lk>
2020-08-08 22:38:42 +0530

    tute 2

---
+++ app/src/main/java/com/example/tute02/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.tute02;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView textVMsg2 = findViewById(R.id.TvMsg2);
+        textVMsg2.setText(R.string.Msg2);
+
+        Log.d( "Lifecycle", "on create...");
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        Log.d( "Lifecycle", "on start...");
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        Log.d( "Lifecycle", "on Resume...");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        Log.d( "Lifecycle", "on Pause...");
+
+    }
+}
